148ebc961ce4da83d0ecfd38fec8fb8301a6ed44
Marco Teti <sgttenore.gmail.com>
2019-04-28 12:35:27 -0700

    formatting for better readability

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,77 @@
 $(document).ready(function () {
   // An array of actions, new actions will be pushed into this array;
-  var topics = ["Huskies", "Cats", "Birds", "Fish"];
+  var topics = ["Huskies", "Cats", "Birds", "Fish", "Wolves",];
   // Creating Functions & Methods
   // Function that displays all gif buttons
-  
+
   function displayInfo() {
     var topic = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic  + "&api_key=HT6SwO59aGj113uijt623dftR6o7lRRt&limit=5";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=HT6SwO59aGj113uijt623dftR6o7lRRt&limit=5";
 
     //use AJAX to GET information on sport button clicked
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response) {
+      url: queryURL,
+      method: "GET"
+    }).done(function (response) {
 
-        //empty sports div so new selection appends to emtpy div - do not want previous searches listed
+      //empty sports div so new selection appends to emtpy div - do not want previous searches listed
 
-        $("#gifs").empty();
+      $("#gifs").empty();
 
-        var results = response.data;
 
-        //user for loop to grab the rating information and appropriate gif for button clicked into its own div to keep information together
+      var results = response.data;
 
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='userSport'>");
+      //user for loop to grab the rating information and appropriate gif for button clicked into its own div to keep information together
 
-            //make variable for rating for clean appending
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='UserGif'>");
 
-            var rating = results[i].rating;
-            var pRate = $("<p>").text("Rating: " + rating);
+        //make variable for rating for clean appending
 
-            //make variables for still url and animated url for clean build
+        var rating = results[i].rating;
+        var pRate = $("<p>").text("Rating: " + rating);
 
-            var urlStill = results[i].images.fixed_height_still.url;
-            var urlPlay = results[i].images.fixed_height.url;
+        //make variables for still url and animated url for clean build
 
-            //gif needs still source to load, and data attributes to store the still and animated gifs for pausing function
+        var urlStill = results[i].images.fixed_height_still.url;
+        var urlPlay = results[i].images.fixed_height.url;
 
-            var gif = $("<img>").addClass("gif").attr("src", urlStill).attr("data-still", urlStill).attr("data-animate", urlPlay).attr("data-state", "still");
+        //gif needs still source to load, and data attributes to store the still and animated gifs for pausing function
 
-            //append the gif and rating to the new div created during for loop
+        var gif = $("<img>").addClass("gif").attr("src", urlStill).attr("data-still", urlStill).attr("data-animate", urlPlay).attr("data-state", "still");
 
-            gifDiv.append(gif);
-            gifDiv.append(pRate);
+        //append the gif and rating to the new div created during for loop
 
-            //append all for loop created divs to the DOM
+        gifDiv.append(gif);
+        gifDiv.append(pRate);
 
-            $("#gifs").append(gifDiv);
+        //append all for loop created divs to the DOM
+
+        $("#gifs").append(gifDiv);
+      }
+
+      //on click of gif still image, gif will play. When clicked again, gif will pause.
+
+      $(".gif").on("click", function () {
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
         }
 
-        //on click of gif still image, gif will play. When clicked again, gif will pause.
-
-        $(".gif").on("click", function() {
-            var state = $(this).attr("data-state");
-
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-            }
-
-        });
+      });
     });
 
-}
+  }
 
-//create buttons out of array indexes - gets information from JSON
+  //create buttons out of array indexes - gets information from JSON
 
-function renderButtons() {
+  function renderButtons() {
 
     //delete original array of buttons everytime renders so they do not keep repeating
 
@@ -80,34 +81,34 @@ function renderButtons() {
 
     for (var i = 0; i < topics.length; i++) {
 
-        var gifRender = $("<button>");
+      var gifRender = $("<button>");
 
-        //add class and attribute of name so display function knows what to GET.
+      //add class and attribute of name so display function knows what to GET.
 
-        gifRender.addClass("topic");
-        gifRender.attr("data-name", topics[i]);
-        gifRender.text(topics[i]);
-        $("#gifButtons").append(gifRender);
+      gifRender.addClass("topic");
+      gifRender.attr("data-name", topics[i]);
+      gifRender.text(topics[i]);
+      $("#gifButtons").append(gifRender);
     }
-}
+  }
 
-//on click event to add an additional sport button when submitted - push input to array.
+  //on click event to add an additional sport button when submitted - push input to array.
 
-$("#addGif").on("click", function(event) {
+  $("#addGif").on("click", function (event) {
     event.preventDefault();
     var sport = $("#gif-input").val().trim();
 
     //push input to original topics array and then rerun render of buttons to show newly added button.
     topics.push(sport);
-        $("#gif-input").val(" ");
+    $("#gif-input").val(" ");
     renderButtons();
-});
+  });
 
 
-//on click entire document to cover all elements named "sport" and run display function
-$(document).on("click", ".topic", displayInfo);
+  //on click entire document to cover all elements named "sport" and run display function
+  $(document).on("click", ".topic", displayInfo);
 
-//run function to display all buttons on startup
-renderButtons();
+  //run function to display all buttons on startup
+  renderButtons();
 
 });
